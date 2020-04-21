98f3ff8f25bd8a82c2ff84e0ada49bef879be287
JaxsonWang <jaxsonswang@gmail.com>
2020-04-21 14:13:34 +0800

    烘焙场景，增加灯光

---
+++ MySteath/Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmLight : MonoBehaviour
+{
+    public float fadeSpeed = 2f;
+    public float hightIntensity = 4f;
+    public float lowIntensity = 0.5f;
+    public float changeMargin = 0.2f;
+    public bool alarmOn;
+    private float targetIntensity;
+    private Light alarmLight;
+
+
+    private void Awake()
+    {
+        alarmLight = GetComponentInParent<Light>();
+        alarmLight.intensity = 0f;
+        targetIntensity = hightIntensity;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alarmOn)
+        {
+            alarmLight.intensity = Mathf.Lerp(alarmLight.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+            CheckTargetIntersity();
+        }
+        else
+        {
+            alarmLight.intensity = Mathf.Lerp(alarmLight.intensity, 0f, fadeSpeed * Time.deltaTime);
+        }
+    }
+
+    void CheckTargetIntersity() 
+    {
+        if (Mathf.Abs(targetIntensity - alarmLight.intensity) < changeMargin)
+        {
+            if (targetIntensity == hightIntensity)
+            {
+                targetIntensity = lowIntensity;
+            }
+            else
+            {
+                targetIntensity = hightIntensity;
+            }
+        }
+    }
+}
