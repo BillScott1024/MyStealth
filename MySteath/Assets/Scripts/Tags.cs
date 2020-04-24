c7260f6a4066af83d5c31d29dc0f2c8a4fe55db1
Akimoto <jaxsonwang@foxmail.com>
2020-04-24 15:04:06 +0800

    更新烘焙场景

---
+++ MySteath/Assets/Scripts/Tags.cs
@@ -0,0 +1,12 @@
+﻿
+
+public static class Tags
+{
+    public static string Player = "PLayer";
+    public static string MainLight = "MainLight";
+    public static string AlermLight = "AlermLight";
+    public static string GameController = "GameController";
+    public static string Fader = "Fader";
+    public static string Enemy = "Enemy";
+    public static string Siren = "Siren";
+}
