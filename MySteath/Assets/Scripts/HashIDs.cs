4c0ce807ab1db2a2ae9ef93405348957aaabb329
Akimoto <jaxsonwang@foxmail.com>
2020-05-10 22:16:04 +0800

    增加动画

---
+++ MySteath/Assets/Scripts/HashIDs.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashIDs : MonoBehaviour
+{
+    //表示 Base Layer 中Dying 状态的 hash id
+    [HideInInspector]
+    public int dyingState;
+    [HideInInspector]
+    public int locomotionState;
+    public int shoutState;
+    public int deadBool;
+    public int speedFloat;
+    public int sneakingBool;
+    public int shoutingBool;
+    public int playerInSightBool;
+    public int shotFloat;
+    public int aimWeightFloat;
+    public int angularSpeedFloat;
+    public int openBool;
+
+
+    private void Awake()
+    {
+        dyingState = Animator.StringToHash("Base Layer.Dying");
+        locomotionState = Animator.StringToHash("Base Layer.Locomotion");
+        shoutState = Animator.StringToHash("Shouting.Shout");
+        deadBool = Animator.StringToHash("Dead");
+        speedFloat = Animator.StringToHash("Speed");
+        sneakingBool = Animator.StringToHash("Sneaking");
+        shoutingBool = Animator.StringToHash("Shouting");
+        playerInSightBool = Animator.StringToHash("PlayerInSight");
+        shotFloat = Animator.StringToHash("Shot");
+        aimWeightFloat = Animator.StringToHash("AimWeight");
+        angularSpeedFloat = Animator.StringToHash("AngularSpeed");
+        openBool = Animator.StringToHash("Open");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MySteath/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public AudioClip shoutingClip;
+    public float turnSmothing = 15f;
+    public float speedDampTime = 0.1f;
+    private Animator animator;
+    private HashIDs hash;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        hash = GameObject.FindWithTag(Tags.GameController).GetComponent<HashIDs>();
+        animator.SetLayerWeight(1, 1f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Rotating(float h, float v)
+    {
+        Vector3 targetDir = new Vector3(h, 0, v);
+        Quaternion targetRotation = Quaternion.LookRotation(targetDir, Vector3.up);
+        Rigidbody r = GetComponent<Rigidbody>();
+        Quaternion newRotation = Quaternion.Lerp(r.rotation, targetRotation, turnSmothing * Time.deltaTime);
+        r.MoveRotation(newRotation);
+    }
+
+    void MovementManagement(float h, float v, bool sneaking)
+    { 
+        animator.SetBool(hash.sneakingBool)
+    }
+}
