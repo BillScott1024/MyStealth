7f0a0e0cc8e7e679f176b8cf7908d93c692788db
Akimoto <jaxsonwang@foxmail.com>
2020-05-12 09:44:38 +0800

    增加Player控制

--- MySteath/Assets/Scripts/PlayerMovement.cs
+++ MySteath/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
-﻿using NUnit.Framework;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -26,7 +25,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool shout = Input.GetButtonDown("Attrack");
+        animator.SetBool(hash.shoutingBool, shout);
+        AudioManagement(shout);
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool sneak = Input.GetButton("Sneak");
+        MovementManagement(h, v, sneak);
     }
 
     void Rotating(float h, float v)
@@ -39,7 +48,38 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void MovementManagement(float h, float v, bool sneaking)
-    { 
-        animator.SetBool(hash.sneakingBool)
+    {
+        animator.SetBool(hash.sneakingBool, sneaking);
+        if (h != 0 || v != 0)
+        {
+            Rotating(h, v);
+            animator.SetFloat(hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
+
+        }
+        else
+        {
+            animator.SetFloat(hash.speedFloat, 0f);
+        }
+
+    }
+
+    void AudioManagement(bool shout)
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if (animator.GetCurrentAnimatorStateInfo(0).nameHash == hash.locomotionState)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if (shout)
+        {
+            AudioSource.PlayClipAtPoint(shoutingClip, transform.position); 
+        }
     }
 }
